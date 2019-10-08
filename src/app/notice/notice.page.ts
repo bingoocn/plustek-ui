4d454c7fa1e7b38bfb5c66591335971c089c8f2e
fengwenhui <fengwenhui5145@163.com>
2019-10-08 09:51:32 +0800

    尝试代码更改

--- src/app/notice/notice.page.ts
+++ src/app/notice/notice.page.ts
@@ -15,7 +15,7 @@ export class NoticePage implements OnInit {
     for(let i=0; i<10; i++) {
       this.notices.push({
         guid: Math.round(Math.random()*10000),
-        title: `201${i}年关于组织召开《精益管理信息化系统培训》的通知`,
+        title: `201${i}年关于组织召开《精益管理信息化系统培训》的通知a`,
         time: `201${i}年10月1日`
       })
     }
