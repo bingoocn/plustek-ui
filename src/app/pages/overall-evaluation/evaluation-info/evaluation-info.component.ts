99037894735ce6203bcaf8ca3ba5b9dc32895f4a
G1tan <827100484@qq.com>
2019-10-24 15:45:35 +0800

    综合评价查看

--- src/app/pages/overall-evaluation/evaluation-info/evaluation-info.component.ts
+++ src/app/pages/overall-evaluation/evaluation-info/evaluation-info.component.ts
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./evaluation-info.component.scss'],
 })
 export class EvaluationInfoComponent implements OnInit {
-
+  public evaluationValue:any = {};
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.evaluationValue = {
+      subGroup:'北化集团',
+      unitName:'245厂',
+      plate:'板块一',
+      selfEvaluationLevel:'五级',
+      selfEvaluationScore:9,
+      checkStartDate:'2019-08-23',
+      checkEndDate:'2019-09-23',
+      pointScore:6,
+    }
+  }
 
 }
