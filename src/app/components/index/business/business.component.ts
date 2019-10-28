8654e709c164586ece8cd45c773c68a4da1b746a
fengwenhui <fengwenhui5145@163.com>
2019-10-28 09:25:58 +0800

    合并代码

--- src/app/components/index/business/business.component.ts
+++ src/app/components/index/business/business.component.ts
@@ -7,8 +7,29 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class BusinessComponent implements OnInit {
   public slideOpts:any = {
-    effect: 'flip', speed: 400, loop:true,autoplay: {delay: 2000}
+    effect: 'flip', 
+    speed: 400, 
+    loop:true, 
+    autoplay: { delay: 2000 }, 
+    pager: false
   }
+
+  public slides = [
+    {
+      self: '5',
+      department: '5',
+      leader: '3'
+    },
+    {
+      department: '5',
+      leader: '3'
+    },
+    {
+      department: '5',
+      leader: '3'
+    }
+  ];
+
   @ViewChild("slide", { static: false }) slide;
   
   constructor() { }
@@ -17,12 +38,6 @@ export class BusinessComponent implements OnInit {
   // 解决手动滑动后轮播图无法正常轮播
   touchEnd() {
     this.slide.startAutoplay();
-  }
-  prev() {
-    this.slide.slidePrev();
-  }
-  next() {
-    this.slide.slideNext();
   }
   // 请求数据完成后如果轮播图工作异常，调用该方法
   update() {
