4bbdde9a283ec272d87a652ca7799b2e42adad16
JessicaXie <15210277136@163.com>
2019-11-02 20:57:44 +0800

    企业自评答题布局

--- src/app/pages/company-assess/questions-page/questions-page.component.ts
+++ src/app/pages/company-assess/questions-page/questions-page.component.ts
@@ -12,41 +12,61 @@ export class QuestionsPageComponent implements OnInit {
 
   public indexes: any = [];
   public indicator_name: string;
-  public indicator_id: string;
-  public evaluationLevelCode: string;
+  public indicator_id: string; //
+  public evaluationLevelCode: string; // 试卷层级
   public evaluationDate: string;
+  public indicatorId: string;
+  public questId: string; //问卷的ID
+
   // public evaluationLevelCode: string;
 
   constructor(public routeInfo: ActivatedRoute, public http: HttpService, private router: Router, public common: CommonService, ) { }
 
   ngOnInit() {
-    // 获取传递过来的规范评价id
-    this.routeInfo.queryParams.subscribe((data) => {this.evaluationLevelCode = data.evaluationLevelCode;this.evaluationDate = data.evaluationDate;});
+    this.routeInfo.queryParams.subscribe((data) => {this.evaluationLevelCode = data.evaluationLevelCode; this.evaluationDate = data.evaluationDate;});
     const params = { index_type_code: '01', start_status_code: '01', evaluation_level_code: this.evaluationLevelCode};
     this.getIndicator(params);
   }
 
-   // 获取当前使用的《规范》评价指标及其下的第一层维度
+   // 获取正在启用的问卷
    getIndicator(params: any) {
-    this.http.getRequest('/indicator_sets', params).then((response: any) => {
-      if (response && response.length > 0) {
-        this.indicator_id = response[0].id;
-        if (response[0].index_name) {
-          if (response[0].year) {
-            this.indicator_name = response[0].index_name;
-          } else {
-            this.indicator_name = response[0].index_name;
-          }
-        }
+     this.http.getRequest(`/questionnaires`,params).then((response: any)=>{
+       this.questId = response[0].id;
+       this.indicator_name = response[0].indicator_sets.index_name;
+       this.http.getRequest(`/questionnaires/${this.questId}/tree`).then((response: any)=>{
         if (response[0].id) {
+          this.indicatorId = response[0].id
           this.http.getRequest('/indicator_sets/' + response[0].id + '/indicators').then(( response: any) => {
             if (response && response.length > 0) {
               this.indexes = this.common.forma2Tree(response, 'pid', 'id');
             }
           })
         }
-      }
-    })
+
+       })
+
+     })
+     
+    // this.http.getRequest('/indicator_sets', params).then((response: any) => {
+    //   if (response && response.length > 0) {
+    //     this.indicator_id = response[0].id;
+    //     if (response[0].index_name) {
+    //       if (response[0].year) {
+    //         this.indicator_name = response[0].index_name;
+    //       } else {
+    //         this.indicator_name = response[0].index_name;
+    //       }
+    //     }
+    //     if (response[0].id) {
+    //       this.indicatorId = response[0].id
+    //       this.http.getRequest('/indicator_sets/' + response[0].id + '/indicators').then(( response: any) => {
+    //         if (response && response.length > 0) {
+    //           this.indexes = this.common.forma2Tree(response, 'pid', 'id');
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   }
 
 }
