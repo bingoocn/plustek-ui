dc17f4d8cc466b15d526be7edc4e236d6e825bc3
JessicaXie <15210277136@163.com>
2019-11-05 20:14:48 +0800

    企业自评回显

--- src/app/pages/company-assess/add-assess/add-assess.component.ts
+++ src/app/pages/company-assess/add-assess/add-assess.component.ts
@@ -9,15 +9,36 @@ import { HttpService } from 'src/app/service/http/http.service';
 })
 export class AddAssessComponent implements OnInit {
 
-  public subordinatePlate: string;
-  public evaluationDate: string;
-  public evaluationLevelCode: string;
+  public subordinatePlate: string;  // 所属板块
+  public unitName: string;  // 单位名
+  public evaluationDate: string;  // 选择日期
+  public evaluationLevelCode: string; // 级次
+  public title: string; // 编辑还是查看是新增
+  public companyId: string; //企业自评ID
 
   constructor(public routeInfo: ActivatedRoute, private router: Router, public http: HttpService) { }
 
   ngOnInit() {
-    // console.log(this.evaluationLevelCode,'日期',
-
+    // 获取登录人信息
+    this.http.getUser().then((response: any) => {
+      if (response && response.subordinateOrgName) {
+        this.unitName = response.subordinateOrgName
+      }
+    })
+    // 获取传递过来的规范评价id
+    this.routeInfo.queryParams.subscribe((data) => {
+      this.companyId = data.companyId,
+        this.title = data.title
+    });
+    if (this.title === 'editor') {
+      this.http.getRequest('/specification_evaluations/' + this.companyId).then((response: any) => {
+        if (response) {
+          this.evaluationLevelCode = response.evaluation_level.code;
+          this.subordinatePlate = response.subordinate_plate;
+          this.evaluationDate = response.evaluation_date;
+        }
+      });
+    }
   }
   // 新增保存答题
   save() {
@@ -30,7 +51,7 @@ export class AddAssessComponent implements OnInit {
   getQuestionnaire() {
     // this.http.getRequest(`/indicator_sets/${this.indicatorId}/indicators`, params).then((response: any) => {
     //   if (response && response.length > 0) {
-       
+
     //   }
     // })
 
@@ -40,7 +61,7 @@ export class AddAssessComponent implements OnInit {
   handleLastNameValue(event) {
     console.log(event);
   }
-  processForm(event) {}
+  processForm(event) { }
 
 
 
