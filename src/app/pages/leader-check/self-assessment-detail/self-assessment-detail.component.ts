0d07e2796b31f05b3f87f31ca4d59b9b1108f23a
xp224848 <xp224848@163.com>
2019-11-05 17:12:49 +0800

    领导审核

--- src/app/pages/leader-check/self-assessment-detail/self-assessment-detail.component.ts
+++ src/app/pages/leader-check/self-assessment-detail/self-assessment-detail.component.ts
@@ -9,20 +9,36 @@ import { HttpService } from 'src/app/service/http/http.service';
 })
 export class SelfAssessmentDetailComponent implements OnInit {
 
-  public assessId: string;
-  public assessInfo: any;
+  public assessId: string; // 企业自评id
+  public assessInfo: any; // 企业自评基本信息
+  public is_checked:boolean; // 分管领导是否已审核
+  public dept_check_info:any; // 部门审核信息
+  public leader_check_info:any; // 分管领导审核信息
 
   constructor(public routeInfo: ActivatedRoute, private router: Router, public http: HttpService) { }
 
   ngOnInit() {
     this.routeInfo.params.subscribe((params: Params) => this.assessId = params['assessId']);
+    this.is_checked = this.routeInfo.snapshot.queryParams['isChecked'];
     if (this.assessId) {
-      // 查询企业自评
+      // 查询企业自评基本信息
       this.http.getRequest('/specification_evaluations/' + this.assessId).then((response: any) => {
         if (response) {
           this.assessInfo = response;
         }
       });
+      // 查询企业自评部门审核信息
+      this.http.getRequest('/specification_evaluations/' + this.assessId + '/department_check').then((response:any) => {
+        if(response){
+          this.dept_check_info = response;
+        }
+      })
+      // 查询企业自评分管领导审核信息
+      this.http.getRequest('/specification_evaluations/' + this.assessId + '/leader_check').then((response:any) => {
+        if(response){
+          this.leader_check_info = response;
+        }
+      })
     }
   }
 
